f3ba262df2c1a6d7220631301281a476e4ebce8f
marcellegrand <marcel.lazodelavega@gmail.com>
2021-11-05 22:01:28 -0500

    Agregando objetos como estructuras

--- clase_20211105/scripts/data_structures.js
+++ clase_20211105/scripts/data_structures.js
@@ -1,3 +1,4 @@
+//ARREGLOS Y MATRICES
 let numeros = [100, 200, 300, 400, 500, 600, 700, 800, 900];
 let nombres = ['marcel', 'rocío', 'paúl', 'lisseth'];
 let matriz = [['zero',1,2,3],0,[0,1,2,3],[4,5,6,7]];
@@ -6,3 +7,17 @@ console.log(numeros[0]);
 console.log(nombres[1]);
 console.log(matriz[2]);
 console.log(matriz[3][2]);
+
+//OBJETOS
+let alumno = {
+    nombre: 'Marcel',
+    apellido: 'Lazo de la Vega',
+    edad: 43,
+    sexo: 'M',
+    getName: function() {
+        return this.nombre;
+    },
+    getFullName: function() {
+        return `${this.nombre}  ${this.apellido}`;
+    }
+};
