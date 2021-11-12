7b411c6b34492218cb0fdd1645b231df85a96ccc
marcellegrand <marcel.lazodelavega@gmail.com>
2021-11-11 22:08:21 -0500

    Agregando import export en clase 2021111

---
+++ clase_20211111/scripts/import.js
@@ -0,0 +1,7 @@
+import {fnSumaForeign, fnRestaForeign, fnMultiplicacionForeign, fnDivisionForeign} from './operations.js';
+
+console.log('Suma: ',fnSumaForeign(8,3));
+console.log('Resta: ',fnRestaForeign(9,4));
+console.log('Multiplicación: ',fnMultiplicacionForeign(2,7));
+console.log('División: ',fnDivisionForeign(10,5));
+
---
+++ clase_20211111/scripts/operations.js
@@ -0,0 +1,15 @@
+export function fnSumaForeign(a,b) {
+    return (a+b);
+}
+
+export function fnRestaForeign(a,b) {
+    return (a-b);
+}
+
+export function fnMultiplicacionForeign(a,b) {
+    return (a*b);
+}
+
+export function fnDivisionForeign(a,b) {
+    return (a/b);
+}
